6349ef36af43fbdf3efcc9c6cb7fc2e8d30287c4
nj_jeong <playjnj@khu.ac.kr>
2023-05-24 02:00:18 +0900

    Modify : uint32 to uint64

--- utils/FrameToJpeg.go
+++ utils/FrameToJpeg.go
@@ -9,7 +9,7 @@ import (
 	ffmpeg_go "github.com/u2takey/ffmpeg-go"
 )
 
-func FrameToJpeg(file *ffmpeg_go.Stream, frameNum int) io.Reader {
+func FrameToJpeg(file *ffmpeg_go.Stream, frameNum uint64) io.Reader {
 	buf := bytes.NewBuffer(nil)
 	err := file.
 		Filter("select", ffmpeg.Args{fmt.Sprintf("gte(n,%d)", frameNum)}).
--- utils/go.mod
+++ utils/go.mod
@@ -8,4 +8,5 @@ require (
 	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
+	golang.org/x/net v0.8.0 // indirect
 )
