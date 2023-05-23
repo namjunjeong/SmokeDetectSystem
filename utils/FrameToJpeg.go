375e2663bfdf8aa2fc71aea1fd05538e95247c3e
nj_jeong <playjnj@khu.ac.kr>
2023-05-23 23:44:25 +0900

    Add : FrameToJpeg.go

---
+++ utils/FrameToJpeg.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+
+	ffmpeg "github.com/u2takey/ffmpeg-go"
+	ffmpeg_go "github.com/u2takey/ffmpeg-go"
+)
+
+func FrameToJpeg(file *ffmpeg_go.Stream, frameNum int) io.Reader {
+	buf := bytes.NewBuffer(nil)
+	err := file.
+		Filter("select", ffmpeg.Args{fmt.Sprintf("gte(n,%d)", frameNum)}).
+		Output("pipe:", ffmpeg.KwArgs{"vframes": 1, "format": "image2", "vcodec": "mjpeg"}).
+		WithOutput(buf, os.Stdout).
+		Run()
+	if err != nil {
+		panic(err)
+	}
+	return buf
+}
