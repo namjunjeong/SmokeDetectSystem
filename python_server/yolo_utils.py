075a69e0dd269f0c3e766fbbdd3aedc8b323ef6f
nj_jeong <playjnj@khu.ac.kr>
2023-05-31 04:27:25 +0900

    Modify : verbose = False

--- python_server/yolo_utils.py
+++ python_server/yolo_utils.py
@@ -2,18 +2,18 @@ from ultralytics import YOLO
 import cv2
 
 class Yolo_Utils_Class():
-    def __init__(self):
-        self.model = YOLO('yolov8n.pt')#define yolo model
+    def __init__(self, model_name):
+        self.model = YOLO(model_name)#define yolo model
         
-    def yolo_box_data(self, pil_img, ind=1, save=False):
-        result = self.model(pil_img)
+    def yolo_predict(self, pil_img, ind=1, with_image=False, save=False):
+        result = self.model.predict(pil_img, verbose=False)
+        plot_img = result[0].plot()
+        data = result[0].boxes.data.tolist()
         
         if save:
-            plot_img = result[0].plot()
             cv2.imwrite(f'{str(ind)}.jpeg',plot_img)#save processed image
-            
-        return result[0].boxes.data.tolist()
-    
-    def yolo_img_data(self, pil_img):
-        result = self.model(pil_img)
-        return result[0].plot()
+
+        if with_image:
+            return (plot_img, data)
+
+        return data
